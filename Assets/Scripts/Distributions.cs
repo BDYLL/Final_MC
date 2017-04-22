dd411c157d2238adb768f2df3bca017ccb7e393f
Diego Yañez <Diego Yaez>
2017-04-22 14:37:23 -0500

    Distributions script

---
+++ Assets/Scripts/Distributions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+public class Distributions{
+
+	private Distributions(){}
+
+	public static double Poisson(double lambda){
+		if (lambda <= 0.0) throw new ArgumentException ();
+		double l = Math.Exp (-lambda);
+		double k = 0.0;
+		double p = 1.0;
+
+
+		Random r = new Random ();
+
+		while (p > l) {
+			k += 1.0;
+			p=p*r.NextDouble ();
+		}
+
+		return k;
+	}
+
+	public static double Exponential(double rate){
+		if (rate <= 0.0) throw new ArgumentException ();
+		double r = new Random ().NextDouble ();
+		return Math.Log (1 - r) / (-rate);
+	}
+}
