9deb045cece1e76b2684fe262ba9903c9125c2a0
Rodolfo <rodolfopv94@gmail.com>
2017-04-22 19:00:44 -0500

    Spawner v5

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
-	public GameObject character;
+	public GameObject[] characters;
 	public float arrivalTime;
 	public float avgServiceTime;
 	public Queue<GameObject> queue;
@@ -11,7 +11,7 @@ public class Spawner : MonoBehaviour {
 	private CharacterBehaviour cb;
     // Use this for initialization
     void Start () {
-		double x = Distributions.Poisson(0.15);
+		double x = Distributions.Poisson(arrivalTime);
 		Debug.Log (x);
 		coroutine = Spawn (2.0f);
 		StartCoroutine (coroutine);
@@ -23,11 +23,12 @@ public class Spawner : MonoBehaviour {
 		while (true)
 		{
 			yield return new WaitForSeconds(waitTime);
+			Debug.Log (Distributions.Poisson (arrivalTime));
 
-			GameObject people = Instantiate (character, this.transform.position, this.transform.rotation);
+			GameObject people = Instantiate (characters[(int)(Random.Range(0.0f, 4.0f))], this.transform.position, this.transform.rotation);
 			cb = people.GetComponent (typeof(CharacterBehaviour)) as CharacterBehaviour;
 			cb.serviceTime = Distributions.Exponential (avgServiceTime);
-			queue.Enqueue (people);
+			queue.Enqueue (people);	
 		}
 	}
 }
