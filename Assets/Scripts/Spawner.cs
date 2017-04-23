3bf5803021cefd178a19732c6d5690581883ba53
Rodolfo <rodolfopv94@gmail.com>
2017-04-22 21:07:07 -0500

    Spawner Completo

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,29 +6,44 @@ public class Spawner : MonoBehaviour {
 	public GameObject[] characters;
 	public float arrivalTime;
 	public float avgServiceTime;
+	public float interval;
 	public Queue<GameObject> queue;
+	private int counter, times;
 	IEnumerator coroutine;
 	private CharacterBehaviour cb;
+	private float rep;
     // Use this for initialization
     void Start () {
 		double x = Distributions.Poisson(arrivalTime);
 		Debug.Log (x);
-		coroutine = Spawn (2.0f);
+		coroutine = Spawn (interval);
 		StartCoroutine (coroutine);
 		queue = new Queue<GameObject> ();
+		counter = 0;
     }
+
+
+	public void createPerson(){
+		if(counter++ < times){
+			GameObject people = Instantiate (characters[(int)(Random.Range(0.0f, 4.0f))], this.transform.position, this.transform.rotation);
+			cb = people.GetComponent (typeof(CharacterBehaviour)) as CharacterBehaviour;
+			cb.serviceTime = Distributions.Exponential (avgServiceTime);
+			queue.Enqueue (people);	
+		}
+		CancelInvoke ();
+		counter = 0;
+	}
 
 	IEnumerator Spawn(float waitTime)
 	{
 		while (true)
 		{
 			yield return new WaitForSeconds(waitTime);
-			Debug.Log (Distributions.Poisson (arrivalTime));
+			times = (int) Distributions.Poisson (arrivalTime);
+			rep = interval / times;
+			Debug.Log (times);
+			InvokeRepeating ("createPerson", 0.0f, rep);
 
-			GameObject people = Instantiate (characters[(int)(Random.Range(0.0f, 4.0f))], this.transform.position, this.transform.rotation);
-			cb = people.GetComponent (typeof(CharacterBehaviour)) as CharacterBehaviour;
-			cb.serviceTime = Distributions.Exponential (avgServiceTime);
-			queue.Enqueue (people);	
 		}
 	}
 }
