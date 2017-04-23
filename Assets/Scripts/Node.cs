c063b48483bf452d787c1d5954ec506b87b91529
Isaac <isaac.morelosc@gmail.com>
2017-04-22 21:52:00 -0500

    Node

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Node : MonoBehaviour {
+
+	public Node[] neighbors;
+	public List<Node> history;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, 0.2f);
+		Gizmos.color = Color.blue;
+		for(int i = 0; i < neighbors.Length; i++){
+			Gizmos.DrawLine (transform.position, neighbors [i].transform.position);
+		}
+	}
+}
